53d380ca648198ba7fe0c969b6d06b439868281a
Simon Fox <simon.fox@drivesoftware.co.nz>
2017-05-16 12:22:16 +1200

    hacking

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,12 @@
-window.define = SystemJS.amdDefine;
-window.require = window.requirejs = SystemJS.amdRequire;
+var path = require('path');
+var gulp = require('gulp');
+var Builder = require('systemjs-builder');
 
-SystemJS.config({
-    map: {
-      "src": "src",
-      "main": "src/main",
+gulp.task('default', function(done) {
+    var builder = new Builder('./scripts/', 'config.js');
 
-      "text": "node_modules/systemjs-plugin-text/text.js",
-      "aurelia-binding":"node_modules/aurelia-binding/dist/system/aurelia-binding",
-      "aurelia-bootstrapper":"../node_modules/aurelia-bootstrapper/dist/system/aurelia-bootstrapper",
-      "aurelia-dependency-injection":"node_modules/aurelia-dependency-injection/dist/system/aurelia-dependency-injection",
-      "aurelia-event-aggregator":"node_modules/aurelia-event-aggregator/dist/system/aurelia-event-aggregator",
-      "aurelia-framework":"node_modules/aurelia-framework/dist/system/aurelia-framework",
-      "aurelia-history":"node_modules/aurelia-history/dist/system/aurelia-history",
-      "aurelia-history-browser":"node_modules/aurelia-history-browser/dist/system/aurelia-history-browser",
-      "aurelia-loader":"node_modules/aurelia-loader/dist/system/aurelia-loader",
-      "aurelia-loader-default":"node_modules/aurelia-loader-default/dist/system/aurelia-loader-default",
-      "aurelia-logging":"node_modules/aurelia-logging/dist/system/aurelia-logging",
-      "aurelia-logging-console":"node_modules/aurelia-logging-console/dist/system/aurelia-logging-console",
-      "aurelia-metadata":"node_modules/aurelia-metadata/dist/system/aurelia-metadata",
-      "aurelia-pal":"node_modules/aurelia-pal/dist/system/aurelia-pal",
-      "aurelia-pal-browser":"node_modules/aurelia-pal-browser/dist/system/aurelia-pal-browser",
-      "aurelia-path":"node_modules/aurelia-path/dist/system/aurelia-path",
-      "aurelia-polyfills":"node_modules/aurelia-polyfills/dist/system/aurelia-polyfills",
-      "aurelia-route-recognizer":"node_modules/aurelia-route-recognizer/dist/system/aurelia-route-recognizer",
-      "aurelia-router":"node_modules/aurelia-router/dist/system/aurelia-router",
-      "aurelia-task-queue":"node_modules/aurelia-task-queue/dist/system/aurelia-task-queue",
-      "aurelia-templating":"node_modules/aurelia-templating/dist/system/aurelia-templating",
-      "aurelia-templating-binding":"node_modules/aurelia-templating-binding/dist/system/aurelia-templating-binding",
-      
-      "aurelia-templating-resources":"node_modules/aurelia-templating-resources/dist/system/aurelia-templating-resources",
-      "aurelia-templating-router":"node_modules/aurelia-templating-router/dist/system/aurelia-templating-router"
-    },
-    packages: {
-      '/': {
-        defaultExtension: 'js'
-      }
-    },
-    meta: {
-      "**/*.html": {
-        loader: "text"
-      }
-    },
-    "bundles": {
-      "../scripts/vendor-bundle.js":[
+builder
+.bundle([
         "../node_modules/aurelia-binding/dist/system/aurelia-binding.js",
         "../node_modules/aurelia-bootstrapper/dist/system/aurelia-bootstrapper.js",
         "../node_modules/aurelia-dependency-injection/dist/system/aurelia-dependency-injection.js",
@@ -102,12 +65,16 @@ SystemJS.config({
         "../node_modules/aurelia-templating-router/dist/system/route-href.js",
         "../node_modules/aurelia-templating-router/dist/system/route-loader.js",
         "../node_modules/aurelia-templating-router/dist/system/router-view.js",
-      ],
-      "../scripts/app-bundle.js": [
-        "src/app.html",
-        "src/app.js",
-        "src/environment.js",
-        "src/main.js"
-      ]
-    }
-  });
+      ], "scripts/vendor-bundle.js")
+.then(function() {
+  console.log('Build complete');
+  done();
+})
+.catch(function(err) {
+  console.log('Build error');
+  console.log(err);
+  done();
+});
+});
+
+
